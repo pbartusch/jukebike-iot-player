25bd506da74cb4f335c6004bfdd381f5d29776b9
Paul Maria Bartusch <paulbartusch@gmx.de>
2020-09-09 11:04:13 +0200

    try/catch around play_next

--- controller.py
+++ controller.py
@@ -15,62 +15,76 @@ CALL_IOT_SETTINGS = '/status'
 
 class JukeController:
 
+    def log_sys_exc(self, in_routine):
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        logger.warn('Exception in {}'.format(in_routine))
+        logger.warn(':: exc_type = {}'.format(exc_type))
+        logger.warn(':: exc_value = {}'.format(exc_value))
+        logger.warn(':: exc_traceback = {}'.format(exc_traceback))
+
     def play_next(self, played_id = None):
         whats_next_prm = ""
-        # TODO distinguish between pure GET vs PUT when accessing with "playedTrack"
         if played_id != None:
             whats_next_prm = "trackPlayed={}".format(played_id)
-        res_whats_next = requests.get('{}{}?{}'.format(API_ROOT, CALL_WHATS_NEXT, whats_next_prm))
-        logger.debug('res_whats_next.status_code = {}'.format(res_whats_next.status_code))
 
-        if (res_whats_next.status_code == 200):
-            new_playlist = res_whats_next.json()
-            logger.debug('new_playlist = {}'.format(json.dumps(new_playlist)))
-            if(len(new_playlist) > 0):
-                self._player.play_track(new_playlist[0])
+        try:
+            # TODO distinguish between pure GET vs PUT when accessing with "playedTrack"
+            res_whats_next = requests.get('{}{}?{}'.format(API_ROOT, CALL_WHATS_NEXT, whats_next_prm))
+            logger.debug('res_whats_next.status_code = {}'.format(res_whats_next.status_code))
+
+            if (res_whats_next.status_code == 200):
+                new_playlist = res_whats_next.json()
+                logger.debug('new_playlist = {}'.format(json.dumps(new_playlist)))
+                if(len(new_playlist) > 0):
+                    self._player.play_track(new_playlist[0])
+                else:
+                    logger.info('play_next: no next song to play ... will requery the cloud in 5 sec!')
+                    time.sleep(5)
+                    self.play_next(played_id)
             else:
-                logger.warn('play_next: no next song to play ... sleeping 5 sec, and requery cloud!')
-                time.sleep(5)
-                self.play_next(played_id)
-        else:
-            logger.warn('res_whats_next got status_code = {} from API'.format(res_whats_next.status_code))
+                logger.warn('res_whats_next got status_code = {} from API'.format(res_whats_next.status_code))
+        except:
+            self.log_sys_exc('play_next')
+            logger.warn('play_next failed for an unknown exception (maybe the connection got lost) ... retrying in 5 sec!')
+            time.sleep(5)
+            self.play_next(played_id)
 
     def notify_track_ended(self, played_id):
-        logger.info('In notify_track_ended')
-        logger.info(':: played_id = {}'.format(played_id))
+        logger.info('In notify_track_ended :: played_id = {}'.format(played_id))
         self.play_next(played_id)
 
-    ## TODO remove - this is only an IoT local sound check!!!
-    #__TMP_volume = 60
-
     def load_iot_settings(self):
-
-        ## TODO remove (see above)
-        #if self.__TMP_volume < 100:
-        #    self.__TMP_volume = self.__TMP_volume+10
-        #else:
-        #    self.__TMP_volume = 60
-        #__TMP_exec_call = "set Digital {}%".format(self.__TMP_volume)
-        #logger.info('load_iot_settings :: exec >>amixer {}'.format(__TMP_exec_call))
-        #subprocess.call(["amixer", "-c", "1", "set", "Digital", "{}%".format(self.__TMP_volume)])
-
         try:
             res_settings = requests.get('{}{}'.format(API_ROOT, CALL_IOT_SETTINGS))
             if (res_settings.status_code == 200):
+                logger.debug("load_iot_settings :: got 200")
                 settings = res_settings.json()
-                if ((self.last_settings is None) | (json.dumps(self.last_settings, sort_keys=True) != json.dumps(settings, sort_keys=True))):
+                logger.debug(":: settings = {}".format(json.dumps(settings)))
+                logger.debug(":: self.last_settings = {}".format(self.last_settings))
+                # TODO refactor!!!
+                b_set_volume = False
+                if (self.last_settings is None):
+                    logger.debug(":: self.last_settings is None")
+                    b_set_volume = True
+                else:
+                    logger.debug(":: self.last_settings is NOT None")
+                    if json.dumps(self.last_settings, sort_keys=True) != json.dumps(settings, sort_keys=True):
+                        logger.debug(":: change in self.last_settings")
+                        b_set_volume = True
+                    else:
+                        logger.debug(":: NO change in self.last_settings")
+                if b_set_volume:
                     # set volume using amixer (from OS)
-                    logger.info('load_iot_settings :: set volume to {}'.format(settings.volume))
-                    # TODO make adressing the sound card configurable or - even better - inferr from JUKEBIKE_CONF
-                    subprocess.call(["amixer", "-c", "1", "set", "Digital", "{}%".format(settings.volume)])
+                    logger.info('load_iot_settings :: set volume to {}'.format(settings['volume']))
+                    # TODO make adressing the sound card configurable or - even better - inferr from current JUKEBIKE_CONF
+                    subprocess.call(["amixer", "-c", "1", "set", "Digital", "{}%".format(settings['volume'])])
                     self.last_settings = settings
                 else:
                     logger.debug('load_iot_settings :: nothing to do!')
             else:
                 logger.warn('load_iot_settings :: res_settings.status_code does not equal 200')
         except:
-            e = sys.exc_info()[0]
-            logger.warn('exception in load_iot_settings :: e = {}'.format(e))
+            self.log_sys_exc('load_iot_settings')
 
     def event_loop(self):
         self.play_next()
